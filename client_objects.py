0757e8aff0d87d083e8e77e3cfaabe1ba01b3be7
unknown <carlos_valverdeb@hotmail.com>
2021-01-14 15:59:53 -0500

    added server_objects and client_objects to test PICKLE for sending python objects

---
+++ client_objects.py
@@ -0,0 +1,28 @@
+# client
+import socket
+import pickle
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+while True:
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(16)      # number of bytes/characters
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("fulll msg recvd")
+            print(full_msg[HEADERSIZE:])
+            d = pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+            new_msg = True
+            full_msg = b''
